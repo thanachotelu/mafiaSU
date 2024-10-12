f81bad05101b117c3a1d75a9ceef54a488456023
kongmanakiattik-p <115211393+kongmanakiattik-p@users.noreply.github.com>
2024-10-13 01:36:46 +0700

    update files

---
+++ First/html/index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MAFIA APPRAISAL</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <div
+      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="d-flex align-items-center justify-content-center w-100">
+        <div class="row justify-content-center w-100">
+          <div class="col-md-8 col-lg-6 col-xxl-3">
+            <div class="card mb-0">
+              <div class="card-body">
+                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                  <img src="../assets/images/logos/appraisal.png" width="180" alt="">
+                </a>
+                <form action="login_cheker.php" method="POST">
+                  <div class="mb-3">
+                      <label for="jobPosition" class="form-label">Job Position</label>
+                      <select class="form-select" id="jobPosition" name="jobPosition" required>
+                          <option value="" disabled selected>Select your job position</option>
+                          <option value="officer">Officer</option>
+                          <option value="maneger">Maneger</option>
+                          <option value="chief">Chief</option>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input  class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                  </div>
+                  <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</button>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
