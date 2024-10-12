f2b14c406ffc48f50dc2001f2f9b98718831f016
Thanachote <keakwanwong_t@silpakorn.edu>
2024-10-12 19:45:49 +0700

    forth commit

--- DB/docker/init.sql
+++ DB/docker/init.sql
@@ -20,27 +20,48 @@ create table employees (
     constraint fk_jobs_id foreign key (job_id) references jobs(job_id)
 );
 
-create table form_appraisal (
-    form_id serial primary key,
-    topic_id INTEGER not null unique,
-    evalutor_id varchar(5) not null,
-    evaluatee_id varchar(5) not null,
-    constraint fk_evalutor foreign key (evalutor_id) references employees(e_id),
-    constraint fk_evaluatee foreign key (evaluatee_id) references employees(e_id)
-);
-
 create table form_topic (
     topic_id INTEGER primary key,
-    topic_name varchar(50) not null,
-    subtopic_id INTEGER not null unique,
-    constraint fk_topic_id foreign key (topic_id) references form_appraisal(topic_id)
+    topic_name varchar(50) not null
 );
 
-create table form_subtopic (
-    subtopic_id INTEGER primary key,
-    subtopic_name varchar(50) not null,
-    scores INTEGER,
-    constraint fk_subtopic_id foreign key (subtopic_id) references form_topic(subtopic_id)
+create table form_appraisal (
+    form_id serial primary key,
+    topic_id INTEGER not null,
+    evaluator_id varchar(5) not null,
+    evaluatee_id varchar(5) not null,
+    feedback varchar(255),
+    constraint fk_evaluator foreign key (evaluator_id) references employees(e_id),
+    constraint fk_evaluatee foreign key (evaluatee_id) references employees(e_id),
+    constraint fk_topic_id foreign key (topic_id) references form_topic(topic_id)
+);
+
+create table form_topic1_info (
+    form_id integer,
+    topic_id integer,
+    job_perform integer not null,
+    quality_of_work integer not null,
+    teamwork integer not null,
+    adaptability_to_change integer not null,
+    time_management integer not null,
+    creativity integer not null,
+    adherence_policies_regulations integer not null,
+    constraint fk_form_id_info1 foreign key (form_id) references form_appraisal(form_id),
+    constraint fk_topic_id_info1 foreign key (topic_id) references form_topic(topic_id)
+);
+
+create table form_topic2_info (
+    form_id integer,
+    topic_id integer,
+    skills_knowledge integer not null,
+    behavior_attiude integer not null,
+    communication integer not null,
+    ability_work_un_press integer not null,
+    leadership integer not null,
+    relationship integer not null,
+    adaptability_learning integer not null,
+    constraint fk_form_id_info2 foreign key (form_id) references form_appraisal(form_id),
+    constraint fk_topic_id_info2 foreign key (topic_id) references form_topic(topic_id)
 );
 
 insert into jobs(job_id,job_name) 
@@ -71,3 +92,24 @@ insert into employees (e_id, firstname, lastname, salary, dept_id, job_id)
         ('00000', 'Thanachote','Keakwanwong', 25000, 1,3),
         ('11111', 'Akarawit','Juntarang', 100000, 1,1),
         ('22222', 'Prompoj','Kongmanakiattikun', 50000,2,2);
+
+insert into form_topic (topic_id,topic_name)
+    values
+        (1,'ประเมินพฤติกรรมการปฏิบัติงาน'),
+        (2,'ประเมิลพฤติกรรมของบุคคล');
+
+insert into form_appraisal (topic_id,evaluator_id,evaluatee_id,feedback)
+    values
+        (1,'11111','00000','ทำงานได้ดีมาก ผมจะเพิ่มเงินเดือนให้คุณ'),
+        (2,'00000','11111','-'),
+        (2,'11111','22222','เป็นบุคคลตัวอย่างที่ดี ต่างกับผมสิ้นเชิง');
+
+
+insert into form_topic1_info 
+    values
+        (1,1,5,5,5,5,5,5,5);
+
+insert into form_topic2_info
+    values
+        (2,2,5,3,3,3,5,5,4),
+        (3,2,5,5,5,5,5,5,5);
