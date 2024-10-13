0c6bef9334f88a8dd8734dec4e4bffa073f35f8b
Thanachote <keakwanwong_t@silpakorn.edu>
2024-10-13 16:34:23 +0700

    release2

---
+++ php/src/html/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "db";
+$username = "appraisal_user";
+$password = "your_strong_password";
+$dbname = "appraisal";
+$port = "5432"; 
+
+try {
+    $conn = new PDO("pgsql:host={$servername};port={$port};dbname={$dbname}", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connection successful!"; // เพิ่มข้อความเพื่อแสดงว่าเชื่อมต่อสำเร็จ
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
